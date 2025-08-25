68f942efc89c08af653b4a0da20518bdd1cae674
Figma beebee <nawgloriawinnyunt_n@cmu.ac.th>
2025-08-25 14:41:04 +0700

    For loops

--- MyFirstProject/Program.cs
+++ MyFirstProject/Program.cs
@@ -8,39 +8,24 @@ namespace MyFirstProject
 {
     internal class Program
     {
+        
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter  a day of the week: ");
-            int day= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("What do you want to repeat?: ");
+            string message = Console.ReadLine();
 
-            
+            Console.WriteLine("How many times do you want to say hi?");
+            int times= Convert.ToInt32(Console.ReadLine());
 
-            switch(day)
+            if (times <= 0)
             {
-                case 1: Console.WriteLine("Mon");
-                    break;
-                case 2: Console.WriteLine("Tue");
-                    break;
-                case 3:
-                    Console.WriteLine("Wed");
-                    break;
-                case 4:
-                    Console.WriteLine("Thu");
-                    break;
-                case 5:
-                    Console.WriteLine("Fri");
-                    break;
-                case 6:
-                    Console.WriteLine("Sat");
-                    break;
-                case 7:
-                    Console.WriteLine("Sun");
-                    break;
-                default:
-                    Console.WriteLine("Invaild, enter a value between 1 and 7");
-                    break;
+                Console.WriteLine("Sorry, please enter a value above 0");
             }
-
+            for (int i = 0; i < times; i++)
+            {
+                Console.WriteLine(message);
+            }
 
         }
     }
