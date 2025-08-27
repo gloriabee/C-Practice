9be2ad689c4e31f59bc2e29cfd2b8c8b562ce4ee
Figma beebee <nawgloriawinnyunt_n@cmu.ac.th>
2025-08-27 17:45:34 +0700

    Try parse function

--- MyFirstProject/Program.cs
+++ MyFirstProject/Program.cs
@@ -13,25 +13,22 @@ namespace MyFirstProject
         
         static void Main(string[] args)
         {
-            double value = 1000D / 12.34D;
-            Console.WriteLine(value);
-            Console.WriteLine(string.Format("{0:0}",value,1000));
-            Console.WriteLine(string.Format("{0:0.#}", value, 1000));
-            Console.WriteLine(string.Format("{0:0.0}", value, 1000));
-            Console.WriteLine(string.Format("{0:0.00}", value, 1000));
+            Console.WriteLine("Enter a number: ");
+            string numInput = Console.ReadLine();
+            int num = 0;
 
-            double money = -10D / 3D;
-            Console.WriteLine(money);
-            Console.WriteLine(string.Format("-$10/$3 = ${0:0.00}", money));
-            Console.WriteLine(money.ToString("C"));
-            Console.WriteLine(money.ToString("C0"));
-            Console.WriteLine(money.ToString("C1"));
-            Console.WriteLine(money.ToString("C2"));
+            bool success=int.TryParse(numInput, out num);
+            if (success)
+            {
+                Console.WriteLine(num);
+            }
+            else
+            {
+                Console.WriteLine("Failed to convert");
+            }
+               
 
-            Console.WriteLine(money.ToString("C", CultureInfo.CurrentCulture));
-            Console.WriteLine(money.ToString("C", CultureInfo.CreateSpecificCulture("en-GB")));
-            Console.WriteLine(money.ToString("C", CultureInfo.CreateSpecificCulture("en-UK")));
-            Console.WriteLine(money.ToString("C", CultureInfo.CreateSpecificCulture("en-AU")));
+
         }
     }
 }
