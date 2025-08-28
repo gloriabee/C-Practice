7a4251eff1db34d9b713e45760c04a4004a7174a
Figma beebee <nawgloriawinnyunt_n@cmu.ac.th>
2025-08-28 17:11:38 +0700

    random number usage

--- MyFirstProject/Program.cs
+++ MyFirstProject/Program.cs
@@ -12,31 +12,12 @@ namespace MyFirstProject
     {
         static void Main(string[] args)
         {
-            List<int> odd = new List<int>();
-            List<int> even = new List<int>();
 
-            for(int i = 0; i <= 20; i++)
-            {
-                if (i % 2 == 0)
-                {
-                    even.Add(i);
-                }
-                else
-                {
-                    odd.Add(i);
-                }
-            }
-            Console.WriteLine("Odd Numbers");
-            foreach (var item in odd)
-            {
-                Console.WriteLine(item+" ");
-            }
-            Console.WriteLine("Even Numbers");
-            foreach (var item in even)
-            {
-               
-                Console.WriteLine(item + " ");
-            }
+            Random random = new Random();
+            int randomNumber = random.Next(1, 11);
+            Console.Write("Random number betwen 1 and 11: ");
+            Console.WriteLine(randomNumber);
+
         }
     }
 }
