261096667dcd0ed2f0c5d8f5afd0775424066750
Figma beebee <nawgloriawinnyunt_n@cmu.ac.th>
2025-08-28 18:09:48 +0700

    Two-dimensional arrays

--- MyFirstProject/Program.cs
+++ MyFirstProject/Program.cs
@@ -12,65 +12,35 @@ namespace MyFirstProject
     {
         static void Main(string[] args)
         {
+            int[,] array2DDeclaration = new int[3, 3];
+            // [0] [0] [0]
+            // [0] [0] [0]
+            // [0] [0] [0]
 
+            int[,,] array3DDeclaration= new int[3,3,3];
 
-            Console.WriteLine("Welcome to the adventure game!");
-            Console.WriteLine("Enter your character's name: ");
-            string playerName=Console.ReadLine();
-            Console.WriteLine("Choose your character type (Warrior,wizard,Archer");
-            string characterType= Console.ReadLine();
+            int[,] array2DInitialized = { {1,2}, {3,4} };
+            // [1] [2]
+            // [3] [4]
 
+            Console.WriteLine(array2DInitialized[0,0]);
+            array2DInitialized[0, 0] = 5;
+            Console.WriteLine(array2DInitialized[0, 0]);
 
-            Console.WriteLine($"You, {playerName} the {characterType} find yourself at the edge of dark forest");
-            Console.WriteLine("Do you enter the forest or camp outside (Enter/Camp)");
-            string choice1= Console.ReadLine();
-
-            if (choice1.ToLower().Equals("enter"))
+            string[,] tictactoe =
             {
-                Console.WriteLine("You bravely enter the forest");
-            }
-            else
-            {
-                Console.WriteLine("You decide to camp out and wait for daylight.");
-
-            }
-
-            bool gameContinue = true;
-
-            while (gameContinue)
-            {
-                Console.WriteLine("You come to a fork in the road. Go left or right?");
-                string direction= Console.ReadLine();
-                if (direction.ToLower().Equals("left"))
                 {
-                    Console.WriteLine("You find a treasure chest!");
-                    gameContinue = false;
-                }
-                else
+                    "O","X","X"
+                },
                 {
-                    Console.WriteLine("You encounter a wild beast!");
-                    Console.WriteLine("Fight or flee? (fight/flee)");
-                    string fightChoice=Console.ReadLine();
-                    if (fightChoice.ToLower().Equals("fight"))
-                    {
-                        Random random=new Random();
-                        int luck = random.Next(1, 11);
-                        if (luck > 5)
-                        {
-                            Console.WriteLine("You beat the wild beast!");
-                            if (luck > 8)
-                            {
-                                Console.WriteLine("The wild beast dropped a treasure");
-                            }
-                        }
-                        else
-                        {
-                            Console.WriteLine("The beast attacked you where you didn't expect");
-                            gameContinue = false;
-                        }
-                    }
+                    "O","O","X"
+                },
+                {
+                    "X","X","O"
                 }
-            }
+            };
+
+            Console.WriteLine(tictactoe[1,2]);
 
 
         }
