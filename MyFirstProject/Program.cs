b444477d885e4cdd56ce82cfd435454297e11ab6
Figma beebee <nawgloriawinnyunt_n@cmu.ac.th>
2025-08-28 17:52:47 +0700

    text adventure game using while loop

--- MyFirstProject/Program.cs
+++ MyFirstProject/Program.cs
@@ -13,10 +13,65 @@ namespace MyFirstProject
         static void Main(string[] args)
         {
 
-            Random random = new Random();
-            int randomNumber = random.Next(1, 11);
-            Console.Write("Random number betwen 1 and 11: ");
-            Console.WriteLine(randomNumber);
+
+            Console.WriteLine("Welcome to the adventure game!");
+            Console.WriteLine("Enter your character's name: ");
+            string playerName=Console.ReadLine();
+            Console.WriteLine("Choose your character type (Warrior,wizard,Archer");
+            string characterType= Console.ReadLine();
+
+
+            Console.WriteLine($"You, {playerName} the {characterType} find yourself at the edge of dark forest");
+            Console.WriteLine("Do you enter the forest or camp outside (Enter/Camp)");
+            string choice1= Console.ReadLine();
+
+            if (choice1.ToLower().Equals("enter"))
+            {
+                Console.WriteLine("You bravely enter the forest");
+            }
+            else
+            {
+                Console.WriteLine("You decide to camp out and wait for daylight.");
+
+            }
+
+            bool gameContinue = true;
+
+            while (gameContinue)
+            {
+                Console.WriteLine("You come to a fork in the road. Go left or right?");
+                string direction= Console.ReadLine();
+                if (direction.ToLower().Equals("left"))
+                {
+                    Console.WriteLine("You find a treasure chest!");
+                    gameContinue = false;
+                }
+                else
+                {
+                    Console.WriteLine("You encounter a wild beast!");
+                    Console.WriteLine("Fight or flee? (fight/flee)");
+                    string fightChoice=Console.ReadLine();
+                    if (fightChoice.ToLower().Equals("fight"))
+                    {
+                        Random random=new Random();
+                        int luck = random.Next(1, 11);
+                        if (luck > 5)
+                        {
+                            Console.WriteLine("You beat the wild beast!");
+                            if (luck > 8)
+                            {
+                                Console.WriteLine("The wild beast dropped a treasure");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("The beast attacked you where you didn't expect");
+                            gameContinue = false;
+                        }
+                    }
+                }
+            }
+
 
         }
     }
