de0a25977ed1cf4977bacd1a48fdb6d199d2cf56
Figma beebee <nawgloriawinnyunt_n@cmu.ac.th>
2025-08-29 23:34:21 +0700

    creating first class

---
+++ MyFirstProject/Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstProject
+{
+    internal class Car
+    {
+        //Constructor
+        public Car() {
+            Console.WriteLine("An object of Car has created");
+        }
+
+
+
+
+
+    }
+}
--- MyFirstProject/Program.cs
+++ MyFirstProject/Program.cs
@@ -11,22 +11,17 @@ namespace MyFirstProject
 
     internal class Program
     {
-        static string name = "Aba";
-       
         static void Main(string[] args)
         {
-            int a = 5, b = 3, sum, product;
-            Calculate(a,b,out sum,out product);
-            Console.WriteLine();
+            //Creating the object of the car
+            Car audi= new Car();
+            Car bmw=new Car();
+          
+
+            Console.ReadKey();
+            
         }
 
-        static void Calculate(int x,int y, out int sum,out int product)
-        {
-            sum = x + y;
-            product = x * y;
-            Console.WriteLine($"Sum: {sum}, Product: {product}");
-        }
-      
     }
 
     }
