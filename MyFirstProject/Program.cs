cc013a94dd449b89275637d2c86392a3daba1cdc
Figma beebee <nawgloriawinnyunt_n@cmu.ac.th>
2025-09-11 17:35:19 +0700

    C# delegates

--- MyFirstProject/Program.cs
+++ MyFirstProject/Program.cs
@@ -15,37 +15,62 @@ namespace MyFirstProject
 
     internal class Program
     {
+        // delegate declaration
+        delegate void PrintDelegate(string text);
 
-        // End of Functions 
+        //calling function inside other functions
+        static void ConnectToDatabase(PrintDelegate log) {
+            // do the insertion
+            log("inserting a new record into the database");
+            log("the record got inserted into the database");
+            // insertion done
+        }
+
+        // static delegate instance
+        static PrintDelegate PrintConsole = (string text) =>
+            Console.WriteLine(text);
+
+        static PrintDelegate PrintToFile = (string text) =>
+        {
+            File.AppendAllText("/logs.txt", text);
+        };
+
+        public delegate void Arithmetic(double num1, double num2);
+        public static void Add(double num1, double num2)
+        {
+            Console.WriteLine($"{num1} + {num2} = {num1+num2}");
+        }
+
+        public static void Subtract(double num1, double num2)
+        {
+            Console.WriteLine($"{num1} - {num2} = {num1-num2}");
+        }
 
         static void Main(string[] args)
+
         {
-            // LINQ Method Syntax
+            PrintConsole("Hello World");
+            ConnectToDatabase(PrintToFile);
+            ConnectToDatabase(PrintConsole);
+            Arithmetic add, sub,addSub;
+            add = new Arithmetic(Add);
+            sub = new Arithmetic(Subtract);
+            addSub = add + sub;
 
-            IList<string> stringList = new List<String>()
-            {
-                              "C# Tutorials",
-                "VB.NET Tutorials",
-                "Learn C++",
-                "MVC Tutorials" ,
-                "Java"
-            };
+            Console.WriteLine($"Add {6} & {10}");
+            add(6, 10);
+            Console.WriteLine($"Add & Subtract {10} & {4}");
+            addSub(10, 4);
 
-            // LINQ Method Syntax 
-
-            var result = stringList.Where(s => s.Contains("Tutorials"));
-
-            foreach (var item in result)
-            {
-                Console.WriteLine(item);
-            }
-            Console.ReadKey();
-
+            Console.ReadLine();
         }
+        }
+       
+        
         
 
     }
-}
+
 
 
 
